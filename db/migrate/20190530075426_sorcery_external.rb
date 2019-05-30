c304df253226e927008265f00983eb9c7c2b78ec
Risa Fujii <risa@kray.jp>
2019-05-30 16:58:21 +0900

    Modify migration files

--- db/migrate/20190530075426_sorcery_external.rb
+++ db/migrate/20190530075426_sorcery_external.rb
@@ -1,12 +1,12 @@
 class SorceryExternal < ActiveRecord::Migration[5.2]
   def change
     create_table :authentications do |t|
-      t.integer :user_id, null: false
+      t.references :user, foreign_key: true
       t.string :provider, :uid, null: false
 
       t.timestamps null: false
     end
 
-    add_index :authentications, [:provider, :uid]
+    add_index :authentications, [:provider, :uid], unique: true
   end
 end
