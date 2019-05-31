40477f10a56d89a4fafd44d997d36e55024f15d1
Risa Fujii <risa@kray.jp>
2019-05-31 12:52:26 +0900

    Callback works now; users can be signed up or signed in

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -2,47 +2,25 @@ class OauthsController < ApplicationController
   skip_before_action :require_login
 
   def oauth
-    login_at(params[:provider])
+    login_at(auth_params[:provider])
   end
 
   def callback
-    # provider = params[:provider]
-    if @user = login_from(params[:provider])
-      redirect_to root_path, notice: "Logged in from #{params[:provider].titleize}!"
+    provider = auth_params[:provider]
+    if @user = login_from(provider)
+      redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
     else
       begin
-        @user = create_from(params[:provider])
+        @user = create_from(provider)
 
         reset_session
         auto_login(@user)
-        redirect_to root_path, notice: "Logged in from #{params[:provider].titleize}!"
+        redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
       rescue StandardError
-        redirect_to root_path, alert: "Failed to login from #{params[:provider].titleize}!"
+        redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
       end
     end
   end
-
-  # def callback
-  #   byebug
-  #   provider = params[:provider]
-  #   if @user = login_from(provider)
-  #     byebug
-  #     redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
-  #   else
-  #     begin
-  #       byebug
-  #       @user = create_from(provider)
-
-  #       reset_session
-  #       auto_login(@user)
-  #       byebug
-  #       redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
-  #     rescue StandardError
-  #       byebug
-  #       redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
-  #     end
-  #   end
-  # end
 
   private
 
