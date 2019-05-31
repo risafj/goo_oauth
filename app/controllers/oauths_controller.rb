27b6491a134565da011493db0a909c7c6fbc24f7
Risa Fujii <risa@kray.jp>
2019-05-31 11:48:37 +0900

    Debugging with byebug

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -5,18 +5,40 @@ class OauthsController < ApplicationController
     login_at(params[:provider])
   end
 
+  # def callback
+  #   provider = params[:provider]
+  #   if @user = login_from(provider)
+  #     redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
+  #   else
+  #     begin
+  #       @user = create_from(provider)
+
+  #       reset_session
+  #       auto_login(@user)
+  #       redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
+  #     rescue StandardError
+  #       redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
+  #     end
+  #   end
+  # end
+
   def callback
+    byebug
     provider = params[:provider]
     if @user = login_from(provider)
+      byebug
       redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
     else
       begin
+        byebug
         @user = create_from(provider)
 
         reset_session
         auto_login(@user)
+        byebug
         redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
       rescue StandardError
+        byebug
         redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
       end
     end
