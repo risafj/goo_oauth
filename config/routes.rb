600989df7b7120db5febd343700091709ebea339
Risa Fujii <risa@kray.jp>
2019-05-31 12:37:49 +0900

    Delete unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'pages/index'
   root to: 'pages#index'
 
-  post 'oauth/callback', to: 'oauths#callback'
   get 'oauth/callback', to: 'oauths#callback'
   get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
 end
