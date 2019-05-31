bc64966eb32ed31927bb64f6d0570ce9977a125c
Risa Fujii <risa@kray.jp>
2019-05-31 12:37:00 +0900

    Callback works if I don't put provider in a variable

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -5,44 +5,44 @@ class OauthsController < ApplicationController
     login_at(params[:provider])
   end
 
+  def callback
+    # provider = params[:provider]
+    if @user = login_from(params[:provider])
+      redirect_to root_path, notice: "Logged in from #{params[:provider].titleize}!"
+    else
+      begin
+        @user = create_from(params[:provider])
+
+        reset_session
+        auto_login(@user)
+        redirect_to root_path, notice: "Logged in from #{params[:provider].titleize}!"
+      rescue StandardError
+        redirect_to root_path, alert: "Failed to login from #{params[:provider].titleize}!"
+      end
+    end
+  end
+
   # def callback
+  #   byebug
   #   provider = params[:provider]
   #   if @user = login_from(provider)
+  #     byebug
   #     redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
   #   else
   #     begin
+  #       byebug
   #       @user = create_from(provider)
 
   #       reset_session
   #       auto_login(@user)
+  #       byebug
   #       redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
   #     rescue StandardError
+  #       byebug
   #       redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
   #     end
   #   end
   # end
-
-  def callback
-    byebug
-    provider = params[:provider]
-    if @user = login_from(provider)
-      byebug
-      redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
-    else
-      begin
-        byebug
-        @user = create_from(provider)
-
-        reset_session
-        auto_login(@user)
-        byebug
-        redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
-      rescue StandardError
-        byebug
-        redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
-      end
-    end
-  end
 
   private
 
