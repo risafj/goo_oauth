854abdb08bd7b4447928204aa6c92ed80f89991a
Risa Fujii <risa@kray.jp>
2019-06-03 15:10:49 +0900

    Change column name to username; email and username both saved to app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_075426) do
+ActiveRecord::Schema.define(version: 2019_06_03_055409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_075426) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "name"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
